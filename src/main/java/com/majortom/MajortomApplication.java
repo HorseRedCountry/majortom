83beb0addc59101b0714f7d81ee644ddbf4e9c11
MajorHorse <mylove103@foxmail.com>
2021-11-10 09:36:53 +0800

    init project

---
+++ src/main/java/com/majortom/MajortomApplication.java
@@ -0,0 +1,16 @@
+package com.majortom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Major Tom
+ */
+@SpringBootApplication
+public class MajortomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajortomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/majortom/MajortomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majortom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajortomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
