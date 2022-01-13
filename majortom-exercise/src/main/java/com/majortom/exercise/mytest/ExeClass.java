670c3687202ca679676de16b23e96950bda0107f
MajorHorse <mylove103@foxmail.com>
2022-01-13 18:45:29 +0800

    update

--- majortom-exercise/src/main/java/com/majortom/exercise/mytest/ExeClass.java
+++ majortom-exercise/src/main/java/com/majortom/exercise/mytest/ExeClass.java
@@ -3,32 +3,45 @@ package com.majortom.exercise.mytest;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
- *  启动类
+ * 启动类
  * </P>
  *
  * @author Major Tom
  * @since 2022/1/12 19:41
  */
 public class ExeClass {
-    public static void main(String[] args){
-        Apple a=Apple.builder()
+    public static void main(String[] args) {
+        Apple a = Apple.builder()
                 .color("red")
                 .name("张三")
-                .year(13)
+                .year(28)
                 .build();
-        Apple b=Apple.builder()
+        Apple b = Apple.builder()
                 .color("green")
                 .name("李四")
                 .year(10)
                 .build();
-        List<Apple> appleList=new ArrayList<>();
+        Apple c = Apple.builder()
+                .color("red")
+                .name("王五")
+                .year(18)
+                .build();
+        List<Apple> appleList = new ArrayList<>();
         appleList.add(a);
         appleList.add(b);
-        appleList.sort(Comparator.comparing(Apple::getYear));
-        appleList.forEach(apple-> System.out.println(apple.toString()));
+        appleList.add(c);
+        appleList.sort(Comparator.comparing(Apple::getColor).thenComparing(Apple::getYear));
+        appleList.forEach(apple -> System.out.println(apple.toString()));
+        System.out.println("===========================================");
+        List<Apple> resultList = appleList.stream().filter(apple -> apple.getColor().equals("red")).collect(Collectors.toList());
+        Apple result = appleList.stream().filter(apple -> apple.getColor().equals("red")).findFirst().orElse(null);
+        System.out.println(result == null ? "没有啊" : result.toString());
+        System.out.println("===========================================");
+        resultList.forEach(apple -> System.out.println(apple.toString()));
     }
 }
 
