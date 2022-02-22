acadcd92e398163107a6d522d53da09461705e83
MajorHorse <mylove103@foxmail.com>
2022-02-22 22:42:22 +0800

    Chapter7练习

---
+++ majortom-exercise/src/main/java/com/majortom/exercise/mytest/java8test/chapter7/Chapter7MainClass.java
@@ -0,0 +1,45 @@
+package com.majortom.exercise.mytest.java8test.chapter7;
+
+
+import com.majortom.exercise.mytest.java8test.chapter7.entity.ParallelStreams;
+
+import java.util.function.Function;
+
+/**
+ * <p>
+ *
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/2/22 19:39
+ */
+public class Chapter7MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Sequential sum done in: " + measureSumPerf(ParallelStreams::sequentialSum, 10_000_000) + "msecs");
+        System.out.println("Parallel sum done in: " + measureSumPerf(ParallelStreams::parallelSum, 10_000_000) + "msecs");
+        System.out.println("Parallel sum done in: " + measureSumPerf(ParallelStreams::iterativeSum, 10_000_000) + "msecs");
+    }
+
+
+    /**
+     * 测量对前n个自然数求和的函数的性能
+     *
+     * @param adder 函数
+     * @param n     前n个数
+     * @return 时间
+     */
+    public static long measureSumPerf(Function<Long, Long> adder, long n) {
+        long fatest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            long sum = adder.apply(n);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            System.out.println("Result: " + sum);
+            if (duration < fatest) {
+                fatest = duration;
+            }
+        }
+        return fatest;
+    }
+}
---
+++ majortom-exercise/src/main/java/com/majortom/exercise/mytest/java8test/chapter7/entity/ParallelStreams.java
@@ -0,0 +1,46 @@
+package com.majortom.exercise.mytest.java8test.chapter7.entity;
+
+import java.util.stream.Stream;
+
+/**
+ * <p>
+ *
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/2/22 19:59
+ */
+public class ParallelStreams {
+    /**
+     * 计算从1到n的所有数字的和
+     *
+     * @param n /
+     * @return /
+     */
+    public static long sequentialSum(long n) {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .reduce(0L, Long::sum);
+    }
+
+    /**
+     * 计算从1到n的所有数字的和--并行流
+     *
+     * @param n /
+     * @return /
+     */
+    public static long parallelSum(long n) {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+    public static long iterativeSum(long n) {
+        long result = 0;
+        for (long i = 0L; i < n; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
