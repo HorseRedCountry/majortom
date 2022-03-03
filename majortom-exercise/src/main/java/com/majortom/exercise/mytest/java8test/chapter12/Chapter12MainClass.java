79f1575e9ad1dc3ade25a71f83122e7e307766f1
MajorHorse <mylove103@foxmail.com>
2022-03-03 22:02:54 +0800

    update

---
+++ majortom-exercise/src/main/java/com/majortom/exercise/mytest/java8test/chapter12/Chapter12MainClass.java
@@ -0,0 +1,18 @@
+package com.majortom.exercise.mytest.java8test.chapter12;
+
+import java.time.LocalDate;
+
+/**
+ * <p>
+ *
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/3/3 21:58
+ */
+public class Chapter12MainClass {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
