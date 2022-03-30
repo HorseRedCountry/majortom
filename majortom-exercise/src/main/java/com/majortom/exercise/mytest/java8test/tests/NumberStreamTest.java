5a3541f793546f41e5b664213a2fc6e6e6eccc8f
MajorHorse <mylove103@foxmail.com>
2022-03-30 10:29:44 +0800

    JSONUtil验证

--- majortom-exercise/src/main/java/com/majortom/exercise/mytest/java8test/tests/NumberStreamTest.java
+++ majortom-exercise/src/main/java/com/majortom/exercise/mytest/java8test/tests/NumberStreamTest.java
@@ -1,5 +1,10 @@
 package com.majortom.exercise.mytest.java8test.tests;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+
 import java.util.stream.IntStream;
 
 /**
@@ -15,5 +20,16 @@ public class NumberStreamTest {
         IntStream.rangeClosed(0, 100)
                 .filter(n -> n % 2 == 0)
                 .forEach(n -> System.out.print(n + "  "));
+        System.out.println();
+        System.out.println(DateTime.now().dayOfMonth());
+
+        String jsonString = "[{\"weight\":70,\"departmentName\":\"PMC部\",\"departmentId\":\"122\"},{\"weight\":30,\"departmentName\":\"工程部\",\"departmentId\":\"125\"}]";
+        JSONArray jsonArray = JSONUtil.parseArray(jsonString);
+        for (Object object : jsonArray) {
+            System.out.println(object);
+            JSONObject jsonObject = JSONUtil.parseObj(object);
+            System.out.println(jsonObject.get("departmentId"));
+        }
+
     }
 }
