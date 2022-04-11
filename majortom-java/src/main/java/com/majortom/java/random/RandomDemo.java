70b888773157f299f52b6c82a5b2622eaa3bb0e7
MajorHorse <mylove103@foxmail.com>
2022-04-11 14:28:44 +0800

    Update Random Deom

---
+++ majortom-java/src/main/java/com/majortom/java/random/RandomDemo.java
@@ -0,0 +1,21 @@
+package com.majortom.java.random;
+
+import cn.hutool.core.util.RandomUtil;
+
+/**
+ * <p>
+ *  随机相关Demo
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/4/11 13:41
+ */
+public class RandomDemo {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 4; i++) {
+            sb.append(RandomUtil.randomInt(10));
+        }
+        System.out.println(sb);
+    }
+}
