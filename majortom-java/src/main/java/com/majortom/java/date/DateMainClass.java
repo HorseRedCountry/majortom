d2bad18730d48033f48719607fd33f39fea79a0a
MajorHorse <mylove103@foxmail.com>
2022-05-31 17:40:31 +0800

    日期工具类Demo

--- majortom-java/src/main/java/com/majortom/java/date/DateMainClass.java
+++ majortom-java/src/main/java/com/majortom/java/date/DateMainClass.java
@@ -1,6 +1,10 @@
 package com.majortom.java.date;
 
+import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.Week;
+
+import java.util.Collections;
 
 /**
  * <p>
@@ -18,5 +22,15 @@ public class DateMainClass {
         for (int i = 0; i < dateArr.length; i++) {
             System.out.println("dateArr[" + i + "] is " + dateArr[i]);
         }
+        System.out.println("===============");
+        char[] chars = DateUtil.dayOfWeekEnum(DateUtil.date()).toChinese().toCharArray();
+        System.out.println(chars[2]);
+        System.out.println(DateUtil.dayOfWeekEnum(DateUtil.date()).toChinese().toCharArray()[2]);
+        String beginDate = "2022-05-30";
+        DateTime dateTime = DateUtil.offsetMonth(DateUtil.parse(beginDate), -1);
+        String lastMonthBeginDay = DateUtil.formatDate(DateUtil.beginOfMonth(DateUtil.parse(DateUtil.format(dateTime, "yyyy-MM-dd"))));
+        String lastMonthEndDay = DateUtil.formatDate(DateUtil.endOfMonth(DateUtil.parse(DateUtil.format(dateTime, "yyyy-MM-dd"))));
+        System.out.println(lastMonthBeginDay);
+        System.out.println(lastMonthEndDay);
     }
 }
