428411a4a2e89bf9ec44fe5bce784caaf3a778bb
MajorHorse <mylove103@foxmail.com>
2022-08-22 19:18:22 +0800

    Demo update

---
+++ majortom-java/src/main/java/com/majortom/java/demo/Demo1.java
@@ -0,0 +1,30 @@
+package com.majortom.java.demo;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * <p>
+ *
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/8/22 10:35
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        List<String> strList= Lists.newArrayList();
+        strList.add("eqweqewq");
+        strList.add("dsfafsd-dd");
+        strList.add("sdfsddf.jpg");
+        String s=strList.toString();
+        String s2= JSONUtil.toJsonStr(strList);
+        String s3 = StrUtil.join(";", strList);
+        System.out.println(s);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+}
