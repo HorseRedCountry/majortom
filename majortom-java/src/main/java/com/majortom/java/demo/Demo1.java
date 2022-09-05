16152beebe47a9ccb869817f9d694f789ba1e6c2
MajorHorse <mylove103@foxmail.com>
2022-09-05 18:29:13 +0800

    Demo update

--- majortom-java/src/main/java/com/majortom/java/demo/Demo1.java
+++ majortom-java/src/main/java/com/majortom/java/demo/Demo1.java
@@ -16,15 +16,24 @@ import java.util.List;
  */
 public class Demo1 {
     public static void main(String[] args) {
-        List<String> strList= Lists.newArrayList();
+        List<String> strList = Lists.newArrayList();
         strList.add("eqweqewq");
         strList.add("dsfafsd-dd");
         strList.add("sdfsddf.jpg");
-        String s=strList.toString();
-        String s2= JSONUtil.toJsonStr(strList);
+        String s = strList.toString();
+        String s2 = JSONUtil.toJsonStr(strList);
         String s3 = StrUtil.join(";", strList);
         System.out.println(s);
         System.out.println(s2);
         System.out.println(s3);
+        System.out.println("********************************");
+        String splitStr = "5";
+        String[] split = splitStr.split(",");
+        for (String s5 : split) {
+            System.out.println("********************************");
+            System.out.println(s5);
+            System.out.println("********************************");
+        }
+        System.out.println("********************************");
     }
 }
