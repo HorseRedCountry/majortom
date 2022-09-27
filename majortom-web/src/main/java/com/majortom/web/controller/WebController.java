5a81d73b1fe078b026ec0758975e720fd46e722b
MajorHorse <mylove103@foxmail.com>
2022-09-27 14:30:53 +0800

    [Demo更新]

--- majortom-web/src/main/java/com/majortom/web/controller/WebController.java
+++ majortom-web/src/main/java/com/majortom/web/controller/WebController.java
@@ -1,11 +1,15 @@
 package com.majortom.web.controller;
 
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
 import com.majortom.web.common.Result;
 import com.majortom.web.pojo.entity.Person;
 import com.majortom.web.service.PersonService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -47,8 +51,40 @@ public class WebController {
     }
 
     @GetMapping("/testOrderBy/{grade}")
-    public List<Person> testOrderBy(@PathVariable(value="grade")String grade) {
+    public List<Person> testOrderBy(@PathVariable(value = "grade") String grade) {
         return personService.testOrderBy(grade);
+    }
+
+    @PostMapping("/import")
+    public void testImport(@RequestBody MultipartFile file) throws IOException {
+        ExcelReader reader = ExcelUtil.getReader(file.getInputStream(), 0);
+        List<List<Object>> objectList = reader.read();
+        for (List<Object> objects : objectList) {
+            //获取模板名称
+            if ("模板名称".equals(objects.get(0).toString())) {
+                System.out.println("模板名称为：" + objects.get(1).toString());
+                continue;
+            }
+            //获取适用人员
+            if ("模板适用人员".equals(objects.get(0).toString())) {
+                System.out.println("模板适用人员为：" + objects.get(1).toString());
+                System.out.println("开始处理模板适用人员.............");
+                continue;
+            }
+            //跳过无用项
+            if ("模板明细".equals(objects.get(0).toString()) || "序号".equals(objects.get(0).toString())) {
+                continue;
+            }
+            //获取并处理模板明细
+            System.out.println("**************************************");
+            System.out.println("序号为：" + objects.get(0).toString());
+            System.out.println("重点工作目标为：" + objects.get(1).toString());
+            System.out.println("协同合作为：" + objects.get(2).toString());
+            System.out.println("资源需求为：" + objects.get(3).toString());
+            System.out.println("目标衡量标准为：" + objects.get(4).toString());
+            System.out.println("**************************************");
+        }
+
     }
 
 }
