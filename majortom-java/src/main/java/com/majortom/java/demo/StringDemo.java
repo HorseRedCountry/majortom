73693bc1d004c3c9b0f7ec4533dd287e57935504
MajorHorse <mylove103@foxmail.com>
2022-09-27 09:13:36 +0800

    [Demo更新]

---
+++ majortom-java/src/main/java/com/majortom/java/demo/StringDemo.java
@@ -0,0 +1,18 @@
+package com.majortom.java.demo;
+
+/**
+ * <p>
+ *
+ * </P>
+ *
+ * @author Major Tom
+ * @since 2022/9/23 16:36
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        String s="sdfs";
+        String s2=new String("sdfs");
+        System.out.println(s.hashCode());
+        System.out.println(s2.hashCode());
+    }
+}
