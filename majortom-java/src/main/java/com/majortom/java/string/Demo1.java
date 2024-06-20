c0fa1c2687f53e67765dc55318c2836eb805fec0
HorseRedCountry <mylove103@foxmail.com>
2024-06-20 16:05:56 +0800

    [Demo Update]

---
+++ majortom-java/src/main/java/com/majortom/java/string/Demo1.java
@@ -0,0 +1,17 @@
+package com.majortom.java.string;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Major Tom
+ * @date 2024/6/20 16:02
+ **/
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("🀎".length());
+        System.out.println("🀎".charAt(0));
+        System.out.println("🀎".charAt(1));
+    }
+}
---
+++ majortom-java/src/main/java/com/majortom/java/threads/ThreadUnSafeExample.java
@@ -0,0 +1,42 @@
+package com.majortom.java.threads;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Major Tom
+ * @date 2024/6/17 15:05
+ **/
+public class ThreadUnSafeExample {
+    private int cnt = 0;
+
+    public void add() {
+        cnt++;
+    }
+
+    public int get() {
+        return cnt;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final int threadSize = 1000;
+        ThreadUnSafeExample example = new ThreadUnSafeExample();
+        CountDownLatch countDownLatch = new CountDownLatch(threadSize);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < threadSize; i++) {
+            executorService.execute(() -> {
+                example.add();
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        System.out.println(example.get());
+    }
+
+}
